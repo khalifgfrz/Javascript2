6c2e0672bbb0bcc1a9e78c187760c4b25e2a12e8
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-04-29 10:53:35 +0700

    revisi

--- Tugas/nomor1.js
+++ Tugas/nomor1.js
@@ -1,6 +1,6 @@
 /**
  * 1. filter
- * filter merupakan salah satu dari method array dan pada umumnya tidak memutasi(mengubah) struktur data aslinya dan hanya menyeleksi hasil dari parameter yang diinginkan.
+ * filter merupakan salah satu dari array method dan pada umumnya tidak memutasi(mengubah) struktur data aslinya dan hanya menyeleksi hasil dari parameter yang diinginkan.
  */
 // Contoh penggunaan filter
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -13,7 +13,7 @@ console.log(evenNumbers);
 
 /**
  * 2. toLowerCase
- * toLowerCase merupakan salah satu dari method string dan pada umumnya tidak memutasi (mengubah) string asli. Sebagai gantinya, metode ini mengembalikan string baru yang berisi salinan string asli dengan semua huruf diubah menjadi huruf kecil. Sehingga, string asli tetap tidak berubah setelah toLowerCase() dipanggil.
+ * toLowerCase merupakan salah satu dari string method dan pada umumnya tidak memutasi (mengubah) string asli. Sebagai gantinya, metode ini mengembalikan string baru yang berisi salinan string asli dengan semua huruf diubah menjadi huruf kecil. Sehingga, string asli tetap tidak berubah setelah toLowerCase() dipanggil.
  */
 // Contoh penggunaan toLowerCase
 let kata = "HELLO WORLD";
@@ -23,7 +23,7 @@ console.log(kataKecil);
 
 /**
  * 3. includes
- * includes merupakan salah satu dari method array dan pada umumnya tidak memutasi (mengubah) array atau string asli, hanya mengembalikan nilai true atau false berdasarkan apakah nilai yang dicari ditemukan dalam array atau string.
+ * includes merupakan salah satu dari array method dan pada umumnya tidak memutasi (mengubah) array atau string asli, hanya mengembalikan nilai true atau false berdasarkan apakah nilai yang dicari ditemukan dalam array atau string.
  */
 // Contoh penggunaan includes
 let buah = ["apel", "pisang", "mangga", "jeruk"];
@@ -33,7 +33,7 @@ console.log(hasil); // Output: true
 
 /**
  * 4. slice
- * slice merupakan salah satu dari method array dan tidak memutasi (mengubah) array atau string asli, hanya mengembalikan array atau string baru yang berisi salinan dari bagian yang dipilih.
+ * slice merupakan salah satu dari array method dan tidak memutasi (mengubah) array atau string asli, hanya mengembalikan array atau string baru yang berisi salinan dari bagian yang dipilih.
  */
 // Contoh penggunaan slice
 let bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni"];
@@ -42,34 +42,32 @@ let slice1 = bulan.slice(2, 5);
 console.log(slice1); // Output: ['Maret', 'April', 'Mei']
 
 /**
- * 5. toUpperCase
- * toUpperCase merupakan salah satu dari method string dan pada umumnya tidak memutasi (mengubah) string asli. Sebagai gantinya, metode ini mengembalikan string baru yang berisi salinan string asli dengan semua huruf diubah menjadi huruf besar. Sehingga, string asli tetap tidak berubah setelah toUpperCase() dipanggil.
+ * 5. unshift
+ * unshift merupakan salah satu dari array method dan memutasi (mengubah) array asli. unshift menambahkan satu atau lebih elemen ke awal array dan menggeser elemen-elemen lainnya ke indeks yang lebih tinggi.
  */
-// Contoh penggunaan toUpperCase
-let hello = "hello world";
-let helloworld = hello.toUpperCase();
-
-console.log(helloworld);
+// Contoh penggunaan unshift
+let array = [2, 3];
+array.unshift(0, 1);
+console.log(array);
 
 /**
- * 6. parseInt
- * parseInt merupakan salah satu dari method number dan tidak memutasi (mengubah) string atau nilai asli, hanya mengembalikan nilai bilangan bulat yang dihasilkan dari konversi string menjadi bilangan bulat sesuai dengan aturan yang berlaku.
+ * 6. indexOf
+ * indexOf merupakan salah satu dari string method dan tidak memutasi (mengubah) string atau array asli, hanya mengembalikan nilai indeks dari nilai yang dicari. Sehingga, array atau string asli tetap tidak berubah setelah indexOf() dipanggil.
  */
-// Contoh penggunaan parseInt
-let desimal = "10,123";
-let angka = parseInt(desimal);
+// Contoh penggunaan indexOf
+let buahan = ["apel", "pisang", "mangga", "jeruk"];
+let indeksBuah = buahan.indexOf("mangga");
 
-console.log(angka);
+console.log(indeksBuah);
 
 /**
- * 7. isNaN
- * isNan merupakan salah satu dari method number dan tidak memutasi (mengubah) nilai asli, hanya mengembalikan nilai true atau false berdasarkan apakah nilai yang diberikan adalah NaN (Not a Number) atau bukan
+ * 7. splice
+ * splice merupakan salah satu dari array method dan memutasi (mengubah) array asli. splice Mengubah konten array dengan menghapus atau mengganti elemen yang ada dan/atau menambahkan elemen baru.
  */
-// Contoh penggunaan isNaN
-let nilai = 123;
-let result = isNaN(nilai);
-
-console.log(result);
+// Contoh penggunaan splice
+let arr = [1, 2, 3, 4, 5];
+arr.splice(2, 1, "a", "b");
+console.log(arr);
 
 /**
  * 8. Math.max
